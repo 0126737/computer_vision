67168c93a17630ae4da87157352cf3fbd3d92f4d
Georg Holasek <georg.holasek@complang.tuwien.ac.at>
2015-12-20 15:38:11 +0100

    Image stitching test -- todo: feathering

--- assignment_2/ass_4/ass4.m
+++ assignment_2/ass_4/ass4.m
@@ -53,8 +53,17 @@ end
 % Step 2 - Choose reference image
 %ref_img = round(size(imgs,4)/2);
 H{1}.tdata.T = H{2}.tdata.T * H{1}.tdata.T;
-H{3}.tdata.T = H{3}.tdata.Tinv;
-H{4}.tdata.T = H{3}.tdata.Tinv * H{4}.tdata.Tinv;
+H{1}.tdata.Tinv = H{2}.tdata.Tinv * H{1}.tdata.Tinv;
+H{4}.tdata.T = H{3}.tdata.T * H{4}.tdata.T;
+H{4}.tdata.Tinv = H{3}.tdata.Tinv * H{4}.tdata.Tinv;
+
+temp = H{3}.tdata.Tinv;
+H{3}.tdata.Tinv = H{3}.tdata.T;
+H{3}.tdata.T = temp;
+
+temp = H{4}.tdata.Tinv;
+H{4}.tdata.Tinv = H{4}.tdata.T;
+H{4}.tdata.T = temp;
 
 % Step 3 - Determine size of panorama image
 corners = zeros(4,2,size(imgs,4)-1);
@@ -63,53 +72,81 @@ for i = 1:2
     corners(:,:,i) = tformfwd(H{i}, [1, 1; ...
                     1, size(imgs(:,:,:,i),1); ...
                     size(imgs(:,:,:,i),2), 1; ...
-                    size(imgs(:,:,:,i),2), size(imgs(:,:,:,1),i)]);
+                    size(imgs(:,:,:,i),2), size(imgs(:,:,:,i),1)]);
 end
 
 for i = 3:4
-    corners(:,:,i) = tforminv(H{i}, [1, 1; ...
-                    1, size(imgs(:,:,:,i),1); ...
-                    size(imgs(:,:,:,i),2), 1; ...
-                    size(imgs(:,:,:,i),2), size(imgs(:,:,:,1),i)]);
+    corners(:,:,i) = tformfwd(H{i}, [1, 1; ...
+                    1, size(imgs(:,:,:,i+1),1); ...
+                    size(imgs(:,:,:,i+1),2), 1; ...
+                    size(imgs(:,:,:,i+1),2), size(imgs(:,:,:,i+1),1)]);
 end
-
 
 max_xy = max(reshape(max(corners),2,4)');
 min_xy = min(reshape(min(corners),2,4)');
-image_size = [ceil(max_xy(2)-min_xy(2)) ceil(max_xy(1)-min_xy(1))];
 
-timg1 = imtransform(imgs(:,:,:,1), H{1}, ...
-       'XData', [corners(1,1,2) max_xy(1)-corners(1,1,2)], ...
-       'YData', [min_xy(2) max_xy(2)], ...
-       'XYScale', [1 1]);
-timg2 = imtransform(imgs(:,:,:,2), H{2}, ...
-       'XData', [min_xy(1) max_xy(1)], ...
-       'YData', [min_xy(2) max_xy(2)], ...
-       'XYScale', [1 1]);
-timg3 = imtransform(imgs(:,:,:,4), H{3}, ...
-       'XData', [min_xy(1) max_xy(1)], ...
-       'YData', [min_xy(2) max_xy(2)], ...
-       'XYScale', [1 1]);
-timg4 = imtransform(imgs(:,:,:,5), H{4}, ...
-       'XData', [min_xy(1) max_xy(1)], ...
-       'YData', [min_xy(2) max_xy(2)], ...
-       'XYScale', [1 1]);
-   
+xdata = [min_xy(1) max_xy(1)];
+ydata = [min_xy(2) max_xy(2)];
+
+timg1 = imtransform(imgs(:,:,:,1), H{1}, 'XData', xdata, 'YData', ydata);
+timg2 = imtransform(imgs(:,:,:,2), H{2}, 'XData', xdata, 'YData', ydata);
+timg_ref = imtransform(imgs(:,:,:,3), maketform('projective',eye(3)), 'XData', xdata, 'YData', ydata);
+timg3 = imtransform(imgs(:,:,:,4), H{3}, 'XData', xdata, 'YData', ydata);
+timg4 = imtransform(imgs(:,:,:,5), H{4}, 'XData', xdata, 'YData', ydata);
+
+canvas = max(timg1, timg2);
+canvas = max(canvas, timg_ref);
+canvas = max(canvas, timg3);
+canvas = max(canvas, timg4);
 figure;
-imshow(timg1);
+imshow(canvas);
+
+canvas2 = timg1 + timg2 + timg_ref + timg3 + timg4;
 figure;
-imshow(timg2);
+imshow(canvas2);
+
+% Feathering
+h = size(img(:,:,:,1),1);
+w = size(img(:,:,:,1),2);
+temp = zeros(h,w);
+temp(:,1) = 1;
+temp(:,w) = 1;
+bw = bwdist(temp);
+bw = bw / max(max(bw));
+
+tbw1 = imtransform(bw, H{1}, 'XData', xdata, 'YData', ydata);
+tbw2 = imtransform(bw, H{2}, 'XData', xdata, 'YData', ydata);
+
+t1 = rgb2gray(timg1)~=0;
+t2 = rgb2gray(timg2)~=0;
+
+map = t1+t2;
+map = map==2;
+
+canvas = timg1+timg2;
+
+R1 = timg1(:,:,1);
+R2 = timg2(:,:,1);
+R = canvas(:,:,1);
+R(map) = R1(map) .* tbw1(map) + R2(map) .* tbw2(map) ./ (tbw1(map) + tbw2(map));
+
+G1 = timg1(:,:,2);
+G2 = timg2(:,:,2);
+G = canvas(:,:,2);
+G(map) = G1(map) .* tbw1(map)+ G2(map) .* tbw2(map) ./ (tbw1(map) + tbw2(map));
+
+B1 = timg1(:,:,3);
+B2 = timg2(:,:,3);
+B = canvas(:,:,3);
+B(map) = B1(map) .* tbw1(map) + B2(map) .* tbw2(map) ./ (tbw1(map) + tbw2(map));
+
+imshow(cat(3, R,G,B));
+
 figure;
-imshow(timg3);
+imshow(tbw1.*map);
+
 figure;
-imshow(timg4);
+imshow(canvas);
 figure;
-imshow(imgs(:,:,:,1));
-figure;
-imshow(imgs(:,:,:,2));
-figure;
-imshow(imgs(:,:,:,3));
-figure;
-imshow(imgs(:,:,:,4));
-figure;
-imshow(imgs(:,:,:,5));
+imshow(map);
+

2781f728523a8dc2ebb5a2f7d140d36870ea3747
Georg Holasek <georg.holasek@complang.tuwien.ac.at>
2015-12-24 12:45:11 +0100

    Completed feathering

--- assignment_2/ass_4/ass4.m
+++ assignment_2/ass_4/ass4.m
@@ -2,7 +2,7 @@
 %
 % Q: Warnings when executing cp2tform?
 % Q: Ignore all errors when executing cp2tform?
-% Q: What about XYScale?
+% Q: panorma image without blending (black borders)?
 %
 % TODO: Repeat B with img2 scaled and rotated
 
@@ -35,118 +35,124 @@ match_plot(img1,img2,points_a(idx,:), points_b(idx,:));
 % Step 5 - transform first image to the second image (Image Registration)
 timg = imtransform(img1, homography, 'XData', [1 size(img2,2)], 'YData', [1 size(img2,1)], 'XYScale', [1 1]);
 
-% C. Image Stitching
+figure;
+imshow(timg);
 
+% C. Image Stitching
 imgs = cat(4, im2double(imread('campus1.jpg')), ...
               im2double(imread('campus2.jpg')), ...
               im2double(imread('campus3.jpg')), ...
               im2double(imread('campus4.jpg')), ...
               im2double(imread('campus5.jpg')));
 
+ref_img = round(size(imgs,4)/2);
+num_imgs = size(imgs,4);
+          
 % Step 1 - Create homographies
-H = cell(1,size(imgs,4)-1);
-for i = 1:size(imgs,4)-1
+H = cell(1,num_imgs);
+for i = 1:ref_img-1
     [points_a, points_b] = matching(single(rgb2gray(imgs(:,:,:,i))), single(rgb2gray(imgs(:,:,:,i+1))));
     H{1,i} = ransac(points_a,points_b, 1000, 5);
 end
+H{1,ref_img} = maketform('projective',eye(3));
+for i = ref_img+1:num_imgs
+    [points_a, points_b] = matching(single(rgb2gray(imgs(:,:,:,i-1))), single(rgb2gray(imgs(:,:,:,i))));
+    H{1,i} = ransac(points_a,points_b, 1000, 5);
+end
 
-% Step 2 - Choose reference image
-%ref_img = round(size(imgs,4)/2);
-H{1}.tdata.T = H{2}.tdata.T * H{1}.tdata.T;
-H{1}.tdata.Tinv = H{2}.tdata.Tinv * H{1}.tdata.Tinv;
-H{4}.tdata.T = H{3}.tdata.T * H{4}.tdata.T;
-H{4}.tdata.Tinv = H{3}.tdata.Tinv * H{4}.tdata.Tinv;
-
-temp = H{3}.tdata.Tinv;
-H{3}.tdata.Tinv = H{3}.tdata.T;
-H{3}.tdata.T = temp;
-
-temp = H{4}.tdata.Tinv;
-H{4}.tdata.Tinv = H{4}.tdata.T;
-H{4}.tdata.T = temp;
+% Step 2 - Align Homographies to reference image
+% Align left images
+for i= ref_img-2:-1:1
+    H{i}.tdata.T = H{i+1}.tdata.T * H{i}.tdata.T;
+    H{i}.tdata.Tinv = H{i+1}.tdata.Tinv * H{i}.tdata.Tinv;    
+end
+% Align right images
+for i = ref_img+2:num_imgs
+    H{i}.tdata.T = H{i-1}.tdata.T * H{i}.tdata.T;
+    H{i}.tdata.Tinv = H{i-1}.tdata.Tinv * H{i}.tdata.Tinv; 
+end
+for i = ref_img+1:num_imgs
+    temp = H{i}.tdata.Tinv;
+    H{i}.tdata.Tinv = H{i}.tdata.T;
+    H{i}.tdata.T = temp;    
+end
 
 % Step 3 - Determine size of panorama image
-corners = zeros(4,2,size(imgs,4)-1);
+corners = zeros(4*num_imgs,2);
+for i = 1:num_imgs
+    corners((i-1)*4+1:i*4,:) = tformfwd(H{i}, [1, 1; ...
+        1, size(imgs(:,:,:,i),1); ...
+        size(imgs(:,:,:,i),2), 1; ...
+        size(imgs(:,:,:,i),2), size(imgs(:,:,:,i),1)]);
+end
+xdata = [min(corners(:,1)) max(corners(:,1))];
+ydata = [min(corners(:,2)) max(corners(:,2))];
 
-for i = 1:2
-    corners(:,:,i) = tformfwd(H{i}, [1, 1; ...
-                    1, size(imgs(:,:,:,i),1); ...
-                    size(imgs(:,:,:,i),2), 1; ...
-                    size(imgs(:,:,:,i),2), size(imgs(:,:,:,i),1)]);
+% Step 4 - Transform images
+timg = cell(1,num_imgs);
+for i=1:num_imgs
+   timg{i} = imtransform(imgs(:,:,:,i), H{i}, 'XData', xdata, 'YData', ydata);
 end
 
-for i = 3:4
-    corners(:,:,i) = tformfwd(H{i}, [1, 1; ...
-                    1, size(imgs(:,:,:,i+1),1); ...
-                    size(imgs(:,:,:,i+1),2), 1; ...
-                    size(imgs(:,:,:,i+1),2), size(imgs(:,:,:,i+1),1)]);
+% Create overlap map
+map = zeros(size(timg{1},1),size(timg{1},2));
+for i=1:num_imgs
+    map = map + (rgb2gray(timg{i})~=0);
 end
+map = map>=2;
+map = cat(3, map, map, map);
 
-max_xy = max(reshape(max(corners),2,4)');
-min_xy = min(reshape(min(corners),2,4)');
-
-xdata = [min_xy(1) max_xy(1)];
-ydata = [min_xy(2) max_xy(2)];
-
-timg1 = imtransform(imgs(:,:,:,1), H{1}, 'XData', xdata, 'YData', ydata);
-timg2 = imtransform(imgs(:,:,:,2), H{2}, 'XData', xdata, 'YData', ydata);
-timg_ref = imtransform(imgs(:,:,:,3), maketform('projective',eye(3)), 'XData', xdata, 'YData', ydata);
-timg3 = imtransform(imgs(:,:,:,4), H{3}, 'XData', xdata, 'YData', ydata);
-timg4 = imtransform(imgs(:,:,:,5), H{4}, 'XData', xdata, 'YData', ydata);
-
-canvas = max(timg1, timg2);
-canvas = max(canvas, timg_ref);
-canvas = max(canvas, timg3);
-canvas = max(canvas, timg4);
+% Show panorama image without blending
+canvas = zeros(size(timg{1}));
+for i = 1:num_imgs
+   if bitget(i,1) % odd
+       canvas = canvas + timg{i};
+   else % even
+       canvas(not(map)) = canvas(not(map)) + timg{i}(not(map));
+   end
+end
 figure;
 imshow(canvas);
 
-canvas2 = timg1 + timg2 + timg_ref + timg3 + timg4;
+% Show panorama image (max of image colors)
+canvas = zeros(size(timg{1}));
+for i = 1:num_imgs
+   canvas = max(canvas, timg{i}); 
+end
 figure;
-imshow(canvas2);
+imshow(canvas);
 
-% Feathering
-h = size(img(:,:,:,1),1);
-w = size(img(:,:,:,1),2);
-temp = zeros(h,w);
-temp(:,1) = 1;
-temp(:,w) = 1;
-bw = bwdist(temp);
-bw = bw / max(max(bw));
+% Step 5 - Feathering
+canvas = zeros(size(timg{1}));
+for i = 1:num_imgs
+    canvas = canvas + timg{i};
+end
 
-tbw1 = imtransform(bw, H{1}, 'XData', xdata, 'YData', ydata);
-tbw2 = imtransform(bw, H{2}, 'XData', xdata, 'YData', ydata);
+% Create alpha channels and transform to plane
+Alpha = cell(1,num_imgs);
+for i=1:num_imgs
+    h = size(imgs(:,:,:,1),1);
+    w = size(imgs(:,:,:,1),2);
+    temp = zeros(h,w);
+    temp(:,1) = 1; temp(:,w) = 1; temp(h,:) = 1; temp(1,:) = 1;
+    Alpha{i} = bwdist(temp);
+    Alpha{i} = Alpha{i} / max(max(Alpha{i}));
+    Alpha{i} = imtransform(Alpha{i}, H{i}, 'XData', xdata, 'YData', ydata);
+end
 
-t1 = rgb2gray(timg1)~=0;
-t2 = rgb2gray(timg2)~=0;
+% Calculate alpha weighted sum of colors
+overlap = cell(1,num_imgs);
+sumlap = zeros(size(canvas));
+sumcolors = zeros(size(canvas));
+for i=1:num_imgs
+    overlap{i} = map .* cat(3,Alpha{i},Alpha{i},Alpha{i});
+    sumcolors = sumcolors + timg{i} .* overlap{i};
+    sumlap = sumlap + overlap{i};
+end
+blended = sumcolors ./ sumlap;
 
-map = t1+t2;
-map = map==2;
-
-canvas = timg1+timg2;
-
-R1 = timg1(:,:,1);
-R2 = timg2(:,:,1);
-R = canvas(:,:,1);
-R(map) = R1(map) .* tbw1(map) + R2(map) .* tbw2(map) ./ (tbw1(map) + tbw2(map));
-
-G1 = timg1(:,:,2);
-G2 = timg2(:,:,2);
-G = canvas(:,:,2);
-G(map) = G1(map) .* tbw1(map)+ G2(map) .* tbw2(map) ./ (tbw1(map) + tbw2(map));
-
-B1 = timg1(:,:,3);
-B2 = timg2(:,:,3);
-B = canvas(:,:,3);
-B(map) = B1(map) .* tbw1(map) + B2(map) .* tbw2(map) ./ (tbw1(map) + tbw2(map));
-
-imshow(cat(3, R,G,B));
-
-figure;
-imshow(tbw1.*map);
+% Set overlapping color values
+canvas(map) = blended(map);
 
 figure;
 imshow(canvas);
-figure;
-imshow(map);
-
